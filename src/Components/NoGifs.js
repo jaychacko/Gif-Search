950111f59c8888b61b55ee9e4169f690bb2359e9
jay chacko <jaychackoui@gmail.com>
2017-04-02 20:15:24 -0500

    gif adding

---
+++ src/Components/NoGifs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NoGifs = props => (
+  <li className='no-gifs'>
+    <i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
+    <h3>Sorry, what you typed, does not Exist!</h3>
+  </li>
+);
+
+export default NoGifs;
